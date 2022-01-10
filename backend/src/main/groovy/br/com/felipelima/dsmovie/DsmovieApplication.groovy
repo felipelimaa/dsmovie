c3eb99d33d7f8494fe0a9ce1847e740d3c748082
Felipe Lima <fearlimasi@gmail.com>
2022-01-10 10:42:52 -0300

    Commit inicial

---
+++ backend/src/main/groovy/br/com/felipelima/dsmovie/DsmovieApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.felipelima.dsmovie
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DsmovieApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DsmovieApplication, args)
+	}
+
+}
